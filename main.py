fd9a98042eee3dc9c187bfb8884527fbc19f691b
eladsez <eladsez36@gmail.com>
2023-04-11 14:38:53 +0300

    networks scaning

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-from scapy.all import ARP, Ether, srp
+from scapy.all import *
 
 
-target_ip = "192.168.1.1/24"
-# IP Address for the destination
-# create ARP packet
-arp = ARP(pdst=target_ip)
-# create the Ether broadcast packet
-# ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
-ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-# stack them
-packet = ether/arp
+def scan_networks():
+    pass
 
-result = srp(packet, timeout=3, verbose=0)[0]
 
-# a list of clients, we will fill this in the upcoming loop
-clients = []
+def scan_clients(ip):
+    # IP Address for the destination
+    # create ARP packet
+    arp = ARP(pdst=ip)
+    # create the Ether broadcast packet
+    # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    # stack them
+    packet = ether / arp
 
-for sent, received in result:
-    # for each response, append ip and mac address to `clients` list
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+    result = srp(packet, timeout=3, verbose=0)[0]
 
-# print clients
-print("Available devices in the network:")
-print("IP" + " "*18+"MAC")
-for client in clients:
-    print("{:16}    {}".format(client['ip'], client['mac']))
+    # a list of clients, we will fill this in the upcoming loop
+    clients = []
 
-# def main():
-#     pass
-#
-# if __name__ == '__main__':
-#     main()
+    for sent, received in result:
+        # for each response, append ip and mac address to `clients` list
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+    # print clients
+    print("Available devices in the network:")
+    print("IP" + " " * 18 + "MAC")
+    for client in clients:
+        print("{:16}    {}".format(client['ip'], client['mac']))
