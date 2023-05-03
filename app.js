97e9cdabe398ded89df98edbebad890ff2195407
EitanKatsightfull <121748185+EitanKatsightfull@users.noreply.github.com>
2023-05-03 20:37:44 +0300

    added cors

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path');
 const logger = require('morgan')
+const cors = require('cors')
+
 
 const app = express()
 const port = process.env.PORT || 8000
 
 
 app.use(logger('dev'))
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
